2fc56bbdbf4308ee629d2a19f1c6a57dd96b4c5a
DP <daulatrao24@gmail.com>
2024-06-08 22:54:37 +0530

    Update: File Structure

---
+++ cmd/rao-say/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	raosay "github.com/RAOdotSH/rao-say/bot"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: raosays <message>")
+		os.Exit(1)
+	}
+
+	message := strings.Join(os.Args[1:], "")
+
+	bot := raosay.Random()
+
+	speechBubble := createSpeechBubble(message)
+
+	fmt.Println(speechBubble)
+	fmt.Println(bot)
+}
+
+func createSpeechBubble(message string) string {
+	lines := strings.Split(message, "\n")
+
+	maxLen := 0
+	for _, line := range lines {
+		if len(line) > maxLen {
+			maxLen = len(line)
+		}
+	}
+
+	border := "+" + strings.Repeat("-", maxLen+2) + "+"
+
+	speechBubble := border + "\n"
+
+	for _, line := range lines {
+		speechBubble += fmt.Sprintf("| %-*s |\n", maxLen, line)
+	}
+
+	speechBubble += border
+
+	return speechBubble
+}
